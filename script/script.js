50c2c1ff1209d015e16b378831f83be1505934d6
Kayky Rodrigues <rodrigueskayky2025@gmail.com>
2025-04-11 21:08:09 -0300

    Iniciando o uso do JavaScript

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+function ClicarIr() {
+    var c2 = document.getElementById('container2')
+    var img = document.getElementById('imagem')
+    var n = document.getElementById('nome')
+    var n2 = document.getElementById('n2')
+
+    n.innerHTML = 'UBIRAJARA'
+}
